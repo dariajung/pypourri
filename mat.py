341d88c76b3a33a5c661b4571b3d0b77e9f3a3c6
Daria Jung <djj2115cu@gmail.com>
2014-12-19 13:02:03 -0500

    90 deg rotation

--- mat.py
+++ mat.py
@@ -16,6 +16,15 @@ def transpose(arr):
     return new_mat
 
 
+def rotate_90(arr):
+    new_mat = transpose(arr)
+    for row in new_mat:
+        last = len(row) - 1
+        row[0], row[last] = row[last], row[0]
+
+    return new_mat
+
+
 if __name__ == "__main__":
     a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
@@ -23,9 +32,16 @@ if __name__ == "__main__":
     # 4 5 6
     # 7 8 9
 
+    # transposed
     # 1 4 7
     # 2 5 8
     # 3 6 9
 
+    # rotated 90 deg
+    # 7 4 1
+    # 8 5 2
+    # 9 6 3
+
     assert(transpose(a)) == [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    assert(rotate_90(a)) == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
     print "test passed"
